135776036c7fa601cecee2f9083cd212c0f3f719
zuryaniskhmc <32119419+zuryaniskhmc@users.noreply.github.com>
2017-11-02 12:05:38 +0800

    tambah file baru index_ppd (page untuk ppd)

---
+++ index_ppd.php
@@ -0,0 +1,57 @@
+<?php include "include/dbconnect.php"; ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+<?php 
+  $page ='home';
+  include 'include/header.php';
+  include 'include/topnav_2.php';
+  include 'include/function.php';
+ ?>
+				
+					
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+              <div class="row">
+                    <div class="col-md-12">
+                     <h2>HALAMAN UTAMA PPD</h2>   
+                        
+                       
+                    </div>
+                </div>
+                 <!-- /. ROW  -->
+<hr />
+              <form>
+  <font style="border-bottom:double">Pengenalan Sistem </font><br>
+  Sistem permohonan ini merangkumi keseluruhan aspek perjalanan proses permohonan dan bertujuan mempermudahkan pengguna membuat permohonan SMART TEAM. selain dari itu, sistem ini juga memudahkan pihak pentadbir JPN memantau perjalanan permohonan sehingga akhir proses. 
+
+
+<br><br>Bagi peringkat permulaan sistem ini hanya akan merangkumi proses permohonan dan diikuti dengan proses-proses lain yang berikutnya.
+
+<br><br><font style="border-bottom:double">Objektif </font><br>
+
+  Menggunakan teknologi masa kini bagi mempermudahkan perhubungan dan mempercepatkan proses permohonan SMART TEAM.
+
+<br><br><font style="border-bottom:double">CARTA ALIR PERMOHONAN BIASA</font><br><br>
+<img src="assets/img/carta_alir.jpg" width="600" height="688">
+</h1>
+  
+                <table width="310">
+                  
+                  
+                </table>
+	</form>
+    </div>
+             <!-- /. PAGE INNER  -->
+             
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <?php include 'include/script.php';?>     
+   
+</body>
+</html>
+
