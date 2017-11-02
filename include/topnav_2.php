796653f66a4c2ab143a8649d1d02f84be8f39341
zuryaniskhmc <32119419+zuryaniskhmc@users.noreply.github.com>
2017-11-02 12:10:05 +0800

    buat perubahan pada menu

--- include/topnav_2.php
+++ include/topnav_2.php
@@ -12,7 +12,7 @@
 				
 					<li><a class="<?php if($page=='home'){echo 'active-menu';}?>" href="index_ppd.php"><i class="fa fa-qrcode fa-3x"></i>Halaman Utama</a></li>
                     <li><a class="<?php if($page=='senarai'){echo 'active-menu';}?>" href="ppd_senarai_permohonan.php"><i class="fa fa-edit fa-3x"></i>Senarai Permohonan</a></li>
-                    <li><a class="<?php if($page=='semak'){echo 'active-menu';}?>"  href="ppd_status_tugas.php"><i class="fa fa-desktop fa-3x"></i>Semak Status Permohonan</a></li>
+                   
 					<li><a onclick="return confirm('Adakah anda pasti untuk daftar keluar?')" href="index.php"><i class="fa fa-square-o fa-3x"  ></i>Daftar Keluar</a></li>							
 					
 				      
