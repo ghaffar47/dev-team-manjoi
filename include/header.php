a84a6bd784e0830788ba080d4aed768884db0c9f
zuryaniskhmc <32119419+zuryaniskhmc@users.noreply.github.com>
2017-11-02 12:20:59 +0800

    perubahan file header

--- include/header.php
+++ include/header.php
@@ -15,17 +15,9 @@
 <body>
 
     <div id="wrapper">
-      <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-            </button>
-                <h6>&nbsp;</h6> 
+      
         </div>
-          <img src="assets/img/BANNER.jpg" width="100%" height="auto">
+          <img src="assets/img/BANNER_2.jpg" width="100%" height="auto">
       </nav>   
 
               
